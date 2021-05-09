24ae32cfea4303a49842bdf0df6b8cac480b15e7
Mohit Nagpal <nagpalm7@gmail.com>
2021-05-09 18:37:06 +0530

    Update courses

---
+++ data/courses.js
@@ -0,0 +1,92 @@
+past_courses = [
+	{
+		"id": 1,
+		"title": "SAP",
+		"description": "Human Capital Management (HCM), Identity Management (IDM), Cloud For Customer (C4C)"
+	},
+	{
+		"id": 2,
+		"title": "Filenet Content Manager – Build Filenet Content Repository",
+		"description": ""
+	},
+	{
+		"id": 3,
+		"title": "Netcool Omnibus (Administration & Maintenance",
+		"description": ""
+	},
+	{
+		"id": 4,
+		"title": "Microsoft - Azure 300, Azure 203, SQL Server",
+		"description": ""
+	},
+	{
+		"id": 5,
+		"title": "Certified Cloud Security Professional(CCSP)",
+		"description": ""
+	},
+	{
+		"id": 6,
+		"title": "Content Navigator",
+		"description": ""
+	},
+	{
+		"id": 7,
+		"title": "Tivoli Network Manager (ITNM)",
+		"description": ""
+	},
+	{
+		"id": 8,
+		"title": "Juniper Security (JSEC)",
+		"description": ""
+	},
+	{
+		"id": 9,
+		"title": "IBM - Manager – Introduction, Filenet Content",
+		"description": ""
+	},
+	{
+		"id": 10,
+		"title": "Tivoli Storage Management (TSM)",
+		"description": ""
+	},
+	{
+		"id": 11,
+		"title": "Application Performance Management (APM)",
+		"description": ""
+	},
+	{
+		"id": 12,
+		"title": "Progress DB",
+		"description": ""
+	},
+	{
+		"id": 12,
+		"title": "SAP Basis",
+		"description": ""
+	},
+	{
+		"id": 12,
+		"title": "SAP Enterprise Portal (EP)",
+		"description": ""
+	},
+	{
+		"id": 12,
+		"title": "SAP Solution Manager IBM AIX",
+		"description": ""
+	},
+	{
+		"id": 12,
+		"title": "Red Hat",
+		"description": ""
+	},
+	{
+		"id": 12,
+		"title": "Java HTML",
+		"description": ""
+	},
+	{
+		"id": 12,
+		"title": "IBM Smart Cloud Desk",
+		"description": ""
+	},
+]
